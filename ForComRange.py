5beb72b199380ca6fd9ebb23826a028faac3bd7c
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-02-10 13:42:00 -0300

    Trabalhando com estrutura FOR utilizando RANGE

---
+++ ForComRange.py
@@ -0,0 +1,6 @@
+
+#realizando um cast para imprimir uma lista
+
+
+c = list(range(0,20,1)) #0 - indica quando inicia o ranger...20- indica quantidade de de números...
+print(c)                # 1- indica a sequencia da impressao
