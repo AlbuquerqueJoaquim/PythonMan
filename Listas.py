254e4dbc4ee42c032212d7d15ce457c56cf60636
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-02-13 18:08:26 -0300

    Trabalhando com lista em python

---
+++ Listas.py
@@ -0,0 +1,7 @@
+#trabalhando com lista
+
+lista = [2,3,5,6,7,8]
+lista.append(12) # adicionando elemento a nossa lista
+del(lista[-1]) # deletando o ultimo elemento adicionado a lista
+lista = 10*[0] # adicionando 10 elementos
+print(lista)
