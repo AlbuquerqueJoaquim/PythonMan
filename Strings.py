8fe484cb0d9d5ae8562d8b0fb0b390010c61db54
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-02-23 11:45:32 -0400

    Trabalhando com string

---
+++ Strings.py
@@ -0,0 +1,12 @@
+
+#trabalhando com string
+
+s1 = 'String com aspas simples'
+s2 = "String com aspas duplas"
+s3 = ''''Stringo com aspas triplas
+com vairas quebras de lilnhas
+entende....'''
+
+print(s1)
+print(s2)
+print(s3)
