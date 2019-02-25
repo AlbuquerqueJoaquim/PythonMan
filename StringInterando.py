69db61d6de47f8a35890bc44fcc727cb7bdc99e3
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-02-25 16:29:12 -0400

    Percorrendo strings utilizando for, while e enumerate

---
+++ StringInterando.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+# percorrendo strings utilizando for, while e enumerate
+
+s = 'Iterando Strings'
+
+#Exemplo1
+
+#for c in s:
+#    print(c)
+
+#Exemplo2
+
+#indice = 0
+#while indice < len(s):
+#    print(indice, s[indice])
+#    indice+=1
+
+#Exemplo3
+
+for k,v in enumerate('Iterando String'):
+    print(k,v)
+
+
