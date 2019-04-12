8a64c9b9b49319122b26c09498089e552d0fd120
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-04-11 23:41:20 -0400

    utilizando a variavel interna do python __all__ para definir quais vaiaveis podem sofrer importação de determinado modulo

---
+++ SimbolosPublicos/ferramentas.py
@@ -0,0 +1,12 @@
+
+#informando quais variavis podem serem impotadas
+# ou quais são publicas e quais são privadas
+__all__ = ["aa","ab"] #variaveis que podem ser importados
+
+
+aa = 0
+ab = 1
+
+#variaveis privadas em um modulo
+ba = 2
+bb = 3
---
+++ SimbolosPublicos/main.py
@@ -0,0 +1,7 @@
+
+
+from ferramentas import *
+
+from pprint import  pprint
+
+pprint(globals())
