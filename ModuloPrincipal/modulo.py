91546c0b7097c12ac97d4a979ddf45957c1725f6
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-04-16 22:05:14 -0400

    Criando um Modulo principal aonde o mesmo não pode ter o nome de palavras reservadas e criando o main da aplicação

---
+++ ModuloPrincipal/modulo.py
@@ -0,0 +1,8 @@
+
+
+#quando um modulo for criado, o mesmo nao podera ter
+# o nome ou a pasta aonde ele se encontra com o nome de uma palavra
+# reservado do python como while, for, int ...
+
+print(__name__)
+
---
+++ ModuloPrincipal/principal.py
@@ -0,0 +1,11 @@
+
+
+#modulo principal
+
+
+from modulo import *
+
+
+#ponto de partida para iniciar uma aplicação no python
+if __name__ == "__main__":
+    print("A aplicação poder inicializada")
