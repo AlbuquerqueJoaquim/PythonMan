e0c98575c63708045952dcb32f87bd66675accf9
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-04-23 22:47:56 -0400

    Trabalhando com exceção

--- ExcecaoValorInvalido.py
+++ ExcecaoValorInvalido.py
@@ -1,7 +1,16 @@
 
 
+def input_float(msg):
+    while True: # enquanto for verdadeiro o codgigo abaixo tem q ser executado
+        try:
+            return float(input(msg)) # convertendo o valor para float
+        except ValueError: # caso o numero não seja valido ira chamar essa except
+            print("Número invalido.")
 
-num1 = float(input("Digite o primeiro numero .:"))
-num2 = float(input("Digite o Segundo numero .:"))
+num1 = input_float("Digite o primeiro numero .:")
+num2 = input_float("Digite o Segundo numero .:")
 
-print(num1/num2)
+try:
+    print(num1/num2)
+except ZeroDivisionError: #caso o numero tenha q ser dividido por zero
+    print("Não é possivel dividir o número por zero.")
