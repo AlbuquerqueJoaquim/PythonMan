87ee4abab2c9f7f6bce1f8d26dfc5cd7b45ca517
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-05-13 22:58:33 -0400

    Realizando encapsulamento!

---
+++ LPOO/RetanguloGettersSetters.py
@@ -0,0 +1,29 @@
+
+#Utilizando os metodos getters e setters (encapsulamento) de dados
+
+class Retangulo:
+
+    def __init__(self, largura, altura):
+        self.largura = 0
+        self.altura = 0
+        self.set_altura(altura)
+        self.set_largura(largura)
+
+
+    def set_altura(self, num):
+        if (not(isinstance(num, int)and(num >0))):
+            raise ValueError("Valor inválido para Altura: {}".format(num))
+        self.altura = num
+
+    def set_largura(self, num):
+        if (not(isinstance(num, int)and(num > 0))):
+            raise ValueError("Valor inválido para Largura: {}".format(num))
+
+        self.largura = num
+
+    def get_area(self):
+        return self.altura * self.largura
+
+
+
+r = Retangulo(largura=9, altura=10)
