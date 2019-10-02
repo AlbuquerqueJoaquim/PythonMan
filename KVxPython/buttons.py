e8d57232e949fa762c72e52dec289c6f7f92ca19
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-10-01 23:36:52 -0400

    Criando arquivo em .kv para ser utilizado pela class python

--- KVxPython/buttons.py
+++ KVxPython/buttons.py
@@ -1,4 +1,5 @@
 
+from tkinter import  Widget
 
 import kivy
 kivy.require('1.9.1')
@@ -7,20 +8,22 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
+
 class Tela1(BoxLayout):
 
     def on_press_bt(self):
+
         janela.root_window.remove_widget(janela.root)
         janela.root_window.add_widget(Tela2())
 
-    def __init__(self, **kwargs):
-        super(Tela1, self).__init__(**kwargs)
-        self.orientation = "vertical"
-        bt1 = Button(text="Clique aqui !")
-        bt1.on_press = self.on_press_bt
-        self.add_widget(bt1)
-        self.add_widget(Button(text="bt2"))
-        self.add_widget(Button(text="bt3"))
+  #  def __init__(self, **kwargs):
+  #      super(Tela1, self).__init__(**kwargs)
+  #      self.orientation = "vertical"
+  #      bt1 = Button(text="Clique aqui !")
+  #      bt1.on_press = self.on_press_bt
+  #      self.add_widget(bt1)
+  #      self.add_widget(Button(text="bt2"))
+  #      self.add_widget(Button(text="bt3"))
 
 class Tela2(BoxLayout):
 
@@ -29,17 +32,17 @@ class Tela2(BoxLayout):
         janela.root_window.remove_widget(janela.root)
         janela.root_window.add_widget(Tela1())
 
-    def __init__(self, **kwargs):
-        super(Tela2, self).__init__(**kwargs)
-        self.orientation = "vertical"
-        bt = Button(text="Clique aqui!!")
-        bt.on_press = self.on_press_bt
-        self.add_widget(bt)
+  #  def __init__(self, **kwargs):
+  #      super(Tela2, self).__init__(**kwargs)
+  #      self.orientation = "vertical"
+  #      bt = Button(text="Clique aqui!!")
+  #      bt.on_press = self.on_press_bt
+  #      self.add_widget(bt)
 
 
-class buttons(App):
+class kvxpython2(App):
     def build(self):
         return Tela1()
 
-janela = buttons()
+janela = kvxpython2()
 janela.run()
