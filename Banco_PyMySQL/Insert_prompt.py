b459dc0b3a521587000a880f4017a58df433ef6a
AlbuquerqueJoaquim <joa_kym_junior@hotmail.com>
2019-12-09 19:02:36 -0400

    Inserindo informações no Banco de dados atraves do Python com MySql. Pegando informalções digitadas pelo usuario no console.

---
+++ Banco_PyMySQL/Insert_prompt.py
@@ -0,0 +1,24 @@
+#importando o package pymysql para realizar a conexao com o banco de dados mysql
+
+import  pymysql.cursors
+
+
+
+conexao = pymysql.connect (
+
+    host='localhost',
+    user='admin',
+    password='Jojo*21021990',
+    db='DBI',
+    cursorclass = pymysql.cursors.DictCursor
+
+)
+# pegando as informações
+x = input('Digite seu nome : ')
+y = input('Digite sua idade : ')
+z = input('Digite seu endereço : ')
+
+# executano o SQL
+with conexao.cursor() as cursor:
+    cursor.execute('insert into pessoa (nome,idade,endereco) values("{}","{}","{}")'.format(x,y,z))
+    conexao.commit()
